7263ece89db495ce50228403a105792a221afbde
devHksingh <hksingh.dev@gmail.com>
2024-12-24 13:52:14 +0530

    add login zod schema

--- src/user/userZodSchema.ts
+++ src/user/userZodSchema.ts
@@ -3,13 +3,20 @@ import { z } from 'zod'
 const createUserSchema = z.object({
     name: z.string().trim(),
     email: z.string().trim().email({ message: 'Invalid email address' }),
-    password: z.string().trim().min(6, { message: "Must be 5 or more characters long" }),
+    password: z.string().trim().min(6, { message: "Must be 6 or more characters long" }),
     confirmPassword:z.string().trim()
 }).refine((data)=> data.password === data.confirmPassword,{
     message: "Passwords don't match",
     path: ["confirmPassword"]
 })
+const loginUserSchema = z.object({
+    
+    email: z.string().trim().email({ message: 'Invalid email address' }),
+    password: z.string().trim().min(6, { message: "Must be 6 or more characters long" }),
+    
+})
 
 export {
-    createUserSchema
+    createUserSchema,
+    loginUserSchema
 }
