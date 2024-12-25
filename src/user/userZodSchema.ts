499f0527528694ea3eda0734952728d7146dc506
devHksingh <hksingh.dev@gmail.com>
2024-12-25 09:07:50 +0530

    add schema for changePassword

--- src/user/userZodSchema.ts
+++ src/user/userZodSchema.ts
@@ -4,19 +4,29 @@ const createUserSchema = z.object({
     name: z.string().trim(),
     email: z.string().trim().email({ message: 'Invalid email address' }),
     password: z.string().trim().min(6, { message: "Must be 6 or more characters long" }),
-    confirmPassword:z.string().trim()
-}).refine((data)=> data.password === data.confirmPassword,{
+    confirmPassword: z.string().trim()
+}).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
     path: ["confirmPassword"]
 })
 const loginUserSchema = z.object({
-    
+
     email: z.string().trim().email({ message: 'Invalid email address' }),
     password: z.string().trim().min(6, { message: "Must be 6 or more characters long" }),
-    
+
 })
+const logouUserSchema = z.object({
+    oldPassword: z.string().trim().min(6, { message: "Must be 6 or more characters long" }),
+    password: z.string().trim().min(6, { message: "Must be 6 or more characters long" }),
+    confirmPassword: z.string().trim()
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"]
+})
+
 
 export {
     createUserSchema,
-    loginUserSchema
+    loginUserSchema,
+    logouUserSchema
 }
