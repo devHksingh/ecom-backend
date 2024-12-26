4c997589a70721f31d6ac58a6eb772a050545799
devHksingh <hksingh.dev@gmail.com>
2024-12-26 14:41:01 +0530

    add product route for register

---
+++ src/product/productRouter.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import multer from 'multer'
+import path from 'node:path'
+import authenticate from '../middlewares/authMiddleware'
+import { createProduct } from './productController'
+
+
+const productRouter = express.Router()
+
+// file store local ->
+const upload = multer({
+    dest: path.resolve(__dirname, "../../public/data/uploads"),
+    // limit 4mb max
+    limits: { fieldSize: 4 * 1024 * 1024 } // max file size to 4mb
+
+})
+
+productRouter.post('/register', authenticate, upload.fields([{ name: "prductImage", maxCount: 1 }]), createProduct)
+
+export default productRouter
