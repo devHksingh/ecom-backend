1b31ae9b7066f7d25663615ef4ca7e64d7e4fd18
devHksingh <hksingh.dev@gmail.com>
2024-12-29 23:18:48 +0530

    add order types

---
+++ src/order/orderTypes.ts
@@ -0,0 +1,9 @@
+export interface Orders {
+    orderStatus: string,
+    trackingId: string,
+    user: [],
+    product: [],
+    orderPlaceOn: Date,
+    totalPrice: number,
+    quantity: number
+}
