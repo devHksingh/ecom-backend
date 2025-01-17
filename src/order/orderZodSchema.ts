f7d90cee8f7854dfdb7792aaf41db1e73096b7cb
devHksingh <hksingh.dev@gmail.com>
2025-01-17 22:52:26 +0530

    add zod schema for updateOrderStatusSchema

--- src/order/orderZodSchema.ts
+++ src/order/orderZodSchema.ts
@@ -2,9 +2,15 @@ import { z } from 'zod'
 
 const placeOrderZodSchema = z.object({
     productId: z.string().trim().min(1, { message: "productId is required" }),
-    quantity: z.number().gt(-1)
+    quantity: z.number().gt(0)
+})
+
+const updateOrderStatusSchema = z.object({
+    trackingId: z.string().trim().min(1, { message: "trackingId is required" }),
+    orderStatus: z.string().trim().min(1, { message: "trackingId is required" })
 })
 
 export {
-    placeOrderZodSchema
+    placeOrderZodSchema,
+    updateOrderStatusSchema
 }
