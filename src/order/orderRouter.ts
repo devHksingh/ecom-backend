ec9dac438985521f2d136e5a466c5cc5fe33c448
devHksingh <hksingh.dev@gmail.com>
2025-01-17 22:53:01 +0530

    add route for updateOrderStatus

--- src/order/orderRouter.ts
+++ src/order/orderRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import authenticate from '../middlewares/authMiddleware'
-import { placeOrder } from './orderController'
+import { placeOrder, updateOrderStatus } from './orderController'
 
 const orderRouter = express.Router()
 
 orderRouter.post('/placeOrder', authenticate, placeOrder)
+orderRouter.post('/updateOrderStatus', authenticate, updateOrderStatus)
 
 export default orderRouter
