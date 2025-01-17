0c4fa0545932e97753476422c4430d3fdbb7a0cd
devHksingh <hksingh.dev@gmail.com>
2025-01-17 23:33:15 +0530

    add route for all order list

--- src/order/orderRouter.ts
+++ src/order/orderRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import authenticate from '../middlewares/authMiddleware'
-import { placeOrder, updateOrderStatus } from './orderController'
+import { getAllOrder, placeOrder, updateOrderStatus } from './orderController'
 
 const orderRouter = express.Router()
 
 orderRouter.post('/placeOrder', authenticate, placeOrder)
 orderRouter.post('/updateOrderStatus', authenticate, updateOrderStatus)
+orderRouter.get('/getAllOrder', authenticate, getAllOrder)
 
 export default orderRouter
