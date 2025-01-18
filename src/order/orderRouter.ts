2baf60ffd4ecf69c96eca5a6677875f8e5cb23a8
devHksingh <hksingh.dev@gmail.com>
2025-01-18 18:00:45 +0530

    add route for getOrderByUserId

--- src/order/orderRouter.ts
+++ src/order/orderRouter.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import authenticate from '../middlewares/authMiddleware'
-import { getAllOrder, getSingleOrder, placeOrder, updateOrderStatus } from './orderController'
+import { getAllOrder, getOrderByUserId, getSingleOrder, placeOrder, updateOrderStatus } from './orderController'
 
 const orderRouter = express.Router()
 
 orderRouter.post('/placeOrder', authenticate, placeOrder)
 orderRouter.post('/updateOrderStatus', authenticate, updateOrderStatus)
 orderRouter.get('/getAllOrder', authenticate, getAllOrder)
+orderRouter.get('/getOrder', authenticate, getOrderByUserId)
 orderRouter.get('/:orderId', getSingleOrder)
 
 export default orderRouter
