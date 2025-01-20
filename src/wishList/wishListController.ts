d3a4cf609cd0a7f1760aeb624e33d3f919c71173
devHksingh <hksingh.dev@gmail.com>
2025-01-20 21:03:04 +0530

    add controller for getWishlist

--- src/wishList/wishListController.ts
+++ src/wishList/wishListController.ts
@@ -75,6 +75,53 @@ const addToWishlist = async (req: Request, res: Response, next: NextFunction) =>
 
 }
 
+const getWishlist = async (req: Request, res: Response, next: NextFunction) => {
+    /*
+    * Authenticate user 
+    * Fetch and populate the wishlist
+    */
+    try {
+        const _req = req as AuthRequest
+        const userId = _req._id
+        const isAccessTokenExp = _req.isAccessTokenExp
+        //    find user
+        const user = await User.findById(userId)
+        if (!user) {
+            return next(createHttpError(400, "User Not found"))
+        }
+        if (!user.isLogin) {
+            return next(createHttpError(400, "Unauthorized request you have to login first"))
+        }
+        const wishlist = await Wishlist.findOne({ user: userId }).populate({
+            path: "products",
+            select: "title price salePrice image totalStock"
+        })
+        // genrate accessToken if expired
+        let accessToken
+        if (isAccessTokenExp) {
+            accessToken = user.generateAccessToken()
+        }
+        if (!wishlist) {
+            return res.status(200).json({
+                success: true,
+                message: "Wishlist is empty",
+                wishlist: null,
+                accessToken: isAccessTokenExp ? accessToken : undefined,
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            wishlist,
+            accessToken: isAccessTokenExp ? accessToken : undefined,
+        });
+    } catch (error) {
+        next(createHttpError(500, "Error fetching wishlist"));
+    }
+
+}
+
 export {
-    addToWishlist
+    addToWishlist,
+    getWishlist
 }
