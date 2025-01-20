8df6151cc19e6c1e84b32c9ab4c0b1ccb6cddd32
devHksingh <hksingh.dev@gmail.com>
2025-01-20 22:41:22 +0530

    add route for removeWishlist

--- src/wishList/wishListRoute.ts
+++ src/wishList/wishListRoute.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import authenticate from '../middlewares/authMiddleware'
-import { addToWishlist, getWishlist } from './wishListController'
+import { addToWishlist, getWishlist, removeWishlist } from './wishListController'
 
 
 const wishListRouter = express.Router()
 
 wishListRouter.get('/getWishlist', authenticate, getWishlist)
+wishListRouter.get('/removeWishlist/:productId', authenticate, removeWishlist)
 wishListRouter.post('/addToWishlist/:productId', authenticate, addToWishlist)
 
 export default wishListRouter
