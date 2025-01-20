d9ba32058b16ffad3311c3721362e5aef9426b55
devHksingh <hksingh.dev@gmail.com>
2025-01-20 20:37:25 +0530

    add controller for addToWishlist

---
+++ src/wishList/wishListController.ts
@@ -0,0 +1,80 @@
+import { Request, Response, NextFunction } from 'express';
+import { Wishlist } from './wishlistModel';
+import mongoose from 'mongoose';
+import { AuthRequest } from '../middlewares/authMiddleware';
+import { User } from '../user/userModel';
+import createHttpError from 'http-errors';
+import { Product } from '../product/productModel';
+
+
+const findOrCreateWishlist = async (userId: mongoose.Schema.Types.ObjectId) => {
+    let wishList = await Wishlist.findOne({ user: userId })
+    if (!wishList) {
+        wishList = await Wishlist.create({ user: userId, products: [] })
+    }
+    return wishList
+}
+
+const addToWishlist = async (req: Request, res: Response, next: NextFunction) => {
+    /*
+    * Check user validity(isLogin,isValidUserId)
+    * Get productId from req.params
+    * Is valid product id 
+    * Add product if not already in wishlist
+    * Populate and return the wishlist
+    */
+    const _req = req as AuthRequest
+    const userId = _req._id
+    const productId = req.params.productId
+    const isAccessTokenExp = _req.isAccessTokenExp
+
+    try {
+        //    find user
+        const user = await User.findById(userId)
+        if (!user) {
+            return next(createHttpError(400, "User Not found"))
+        }
+        if (!user.isLogin) {
+            return next(createHttpError(400, "Unauthorized request you have to login first"))
+        }
+        // Check product exists
+        const product = await Product.findById(productId)
+        if (!product) {
+            return next(createHttpError(404, "Product not found"));
+        }
+        // Find or create wishlist
+        const wishList = await findOrCreateWishlist(user.id)
+
+        // Add product if ist not in wishlist
+
+        if (!wishList.products.includes(product.id)) {
+            wishList.products.push(product.id)
+            await wishList.save()
+        }
+        // Populate and return the wishlist
+
+        const populatedWishlist = await Wishlist.findById(wishList.id).populate({
+            path: "products",
+            select: "title price salePrice image totalStock"
+        })
+        // genrate accessToken if expired
+        let accessToken
+        if (isAccessTokenExp) {
+            accessToken = user.generateAccessToken()
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Product added to wishlist",
+            wishlist: populatedWishlist,
+            accessToken: isAccessTokenExp ? accessToken : undefined,
+        });
+    } catch (error) {
+        next(createHttpError(500, "Error adding to wishlist"));
+    }
+
+}
+
+export {
+    addToWishlist
+}
