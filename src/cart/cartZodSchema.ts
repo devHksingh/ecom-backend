59db62de04bac87e7e2c3e778b70c0ea41b750af
devHksingh <hksingh.dev@gmail.com>
2025-02-06 11:52:20 +0530

    add zod schema for update cart

--- src/cart/cartZodSchema.ts
+++ src/cart/cartZodSchema.ts
@@ -1,6 +1,7 @@
-import mongoose from 'mongoose'
-import z from 'zod'
+import mongoose from 'mongoose';
+import z from 'zod';
 
+const action = ["add", "remove"] as const;
 
 const cartZodSchema = z.object({
     productId: z.string().trim().min(1, { message: "productId is required" })
@@ -8,6 +9,15 @@ const cartZodSchema = z.object({
             message: "Invalid MongoDB ObjectId"
         }),
     quantity: z.number().gt(0)
-})
+});
 
-export { cartZodSchema }
+const cartUpdatequantity = z.object({
+    productId: z.string().trim().min(1, { message: "productId is required" })
+        .refine(value => mongoose.Types.ObjectId.isValid(value), {
+            message: "Invalid MongoDB ObjectId"
+        }),
+    quantity: z.number().gt(0),
+    type: z.enum(action)
+});
+
+export { cartZodSchema, cartUpdatequantity };
