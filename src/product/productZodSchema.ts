fde839af0563c8ef81cbfbb7cf703d45c2c392a5
devHksingh <hksingh.dev@gmail.com>
2025-02-10 18:54:43 +0530

    add coerce in zod

--- src/product/productZodSchema.ts
+++ src/product/productZodSchema.ts
@@ -7,10 +7,10 @@ const createProductSchema = z.object({
     brand: z.string().min(4, { message: "Must be 4 or more characters long" }),
     category: z.string().min(2, { message: "Must be 2 or more characters long" }),
     // image:z.string().min(8,{ message: "Must be 8 or more characters long" }),
-    price: z.number().gt(-1),
+    price: z.coerce.number().gt(-1),
     currency: z.string().min(2, { message: "Must be 2 or more characters long" }),
-    salePrice: z.number().min(0),
-    totalStock: z.number().min(0)
+    salePrice: z.coerce.number().min(0),
+    totalStock: z.coerce.number().min(0)
 })
 
 export { createProductSchema }
