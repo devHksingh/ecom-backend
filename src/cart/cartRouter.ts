9032856faa911f3faecde239151bd422540dbef2
devHksingh <hksingh.dev@gmail.com>
2025-04-25 22:15:47 +0530

    add route for multipleProductAddToCart

--- src/cart/cartRouter.ts
+++ src/cart/cartRouter.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import authenticate from "../middlewares/authMiddleware"
-import { addToCart, getCart, removeFromCart, updateCartQuantity } from "./cartController"
+import { addToCart, getCart, multipleProductAddToCart, removeFromCart, updateCartQuantity } from "./cartController"
 
 const cartRouter = express.Router()
 
 
 cartRouter.post('/addCartProduct', authenticate, addToCart)
+cartRouter.post('/multilpeProductAddToCart', authenticate, multipleProductAddToCart)
 cartRouter.post('/updateCartProduct', authenticate, updateCartQuantity)
 cartRouter.delete('/removeFromCart/:productId', authenticate, removeFromCart)
 cartRouter.get('/getCart', authenticate, getCart)
